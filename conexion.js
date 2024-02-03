4911ac904ce5ded438a84cb92da9710de29dad0d
gcruzrios <gcruzr@gmail.com>
2024-02-02 19:33:58 -0600

    segundo commit

--- conexion.js
+++ conexion.js
@@ -4,19 +4,9 @@ main().catch(err => console.log(err));
 
 URI=process.env.DB_CNN;
 
-// mongoose.connect(URI, {
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true,
-//     useCreateIndex:true,
-//     useFindAndModify:false
-// }).then(db=> console.log('Base de datos Conectada'))
-//   .catch(error=>console.log(error))
-
-
-
 async function main() {
   
-  //await mongoose.connect('mongodb+srv://gcruzrios:Grvn240675@cluster0.c5fgm.mongodb.net/crudmernDb');
+  //await mongoose.connect('mongodb+srv://gcruzrios:Grvn240675@cluster0.c5fgm.mongodb.net/NodejsDb');
   await mongoose.connect('mongodb://127.0.0.1:27017/NodejsDb');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
